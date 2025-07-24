922d904fbdca4b9e95e92368c3f3e77607dfcec8
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-24 20:17:19 +0300

    положил начало проекта: добавил минимальный парсер

--- script/parser.py
+++ script/parser.py
@@ -1,3 +1,16 @@
-"""
-Файл для парсера.
-"""
+import argparse
+
+
+def parser_args():
+    """
+    Парсит аргументы командной строки для Лог-репорт.
+
+    Returns:
+        argparse.Namespace: Пространство имен с аргументами командной строки.
+    """
+    parser = argparse.ArgumentParser(description="Лог-репорт")
+    parser.add_argument(
+        "logfile",
+        help="Путь к JSON-файлу с логами",
+    )
+    return parser.parse_args()
