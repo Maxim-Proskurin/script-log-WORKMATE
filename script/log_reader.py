1db16f25dd3faa64d64477283ca6e2514ac93cb2
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-24 22:42:00 +0300

    add: лог-ридер(читает логи из файла)

--- script/log_reader.py
+++ script/log_reader.py
@@ -1,3 +1,21 @@
-"""
-Файл для чтения логов.
-"""
+import json
+from typing import Any, Dict, List
+
+
+def read_logs(filepath: str) -> List[Dict[str, Any]]:
+    """
+    Читает логи из JSON-файла.
+
+    Args:
+        filepath (str): Путь к JSON-файлу(ам).
+
+    Returns:
+        List[Dict[str, Any]]: Список логов (каждый лог словарь).
+    """
+    logs = []
+    with open(file=filepath, mode="r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                logs.append(json.loads(line))
+    return logs
